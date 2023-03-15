45ddc0e8a3f20c69d9599d72b5c874ab00b71cfd
Multyjog <simon.kotov.web@gmail.com>
2023-03-16 01:23:16 +0300

    adding dist subtree

---
+++ dist/precache-manifest.af8cfe0d126125b17c7cc0198cd27ae4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd38708e3fbd9a24b87",
+    "url": "/test-pwa-application/css/app.13079f40.css"
+  },
+  {
+    "revision": "af756fa13554220e85d2f1ba73ad1cbc",
+    "url": "/test-pwa-application/data/documents.json"
+  },
+  {
+    "revision": "ee875af15e7691cb99bee97baf3d20df",
+    "url": "/test-pwa-application/data/files/convert.docx"
+  },
+  {
+    "revision": "5e084ad4cf595e0594d5071d067e728a",
+    "url": "/test-pwa-application/data/files/document.doc"
+  },
+  {
+    "revision": "c2acd3c2b696fd075fe796845ec8846b",
+    "url": "/test-pwa-application/index.html"
+  },
+  {
+    "revision": "4fd38708e3fbd9a24b87",
+    "url": "/test-pwa-application/js/app.5f492399.js"
+  },
+  {
+    "revision": "528ca498edbe13f9e34e",
+    "url": "/test-pwa-application/js/chunk-vendors.e930dcfa.js"
+  },
+  {
+    "revision": "f0f93dc42347fe2e3d8f3bccf085e1c4",
+    "url": "/test-pwa-application/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/test-pwa-application/robots.txt"
+  }
+]);
